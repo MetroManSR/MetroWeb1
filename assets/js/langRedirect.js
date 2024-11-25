a4cb92aa7807d6562ef54f1c0a07205629608eb4
MetroMan <services@metroman.me>
2024-11-25 10:56:56 -0300

    😈 langRedirect.js

--- assets/js/langRedirect.js
+++ assets/js/langRedirect.js
@@ -1,10 +1,20 @@
-function redirect() {
+function langRedirect(sourceLang) {
     // Get the current path
     let path = window.location.pathname;
 
+    let newPath = "" 
+    
     // Add "en/" at the beginning of the path
-    let newPath = 'en/' + path;
+    if (sourceLang == "es") {
+    
+    newPath = '/en' + path;
+   
+    } else {
 
+    newPath = path.replace("en/", "") 
+     
+    } 
+    
     // Construct the new URL
     let newUrl = window.location.origin + newPath;
 
