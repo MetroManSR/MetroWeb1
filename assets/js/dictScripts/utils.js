44df6a144299f0681e67beb5bf07c59a4dd40359
MetroMan <services@metroman.me>
2024-12-06 15:08:10 -0300

    Update utils.js

--- assets/js/dictScripts/utils.js
+++ assets/js/dictScripts/utils.js
@@ -2,14 +2,14 @@ export function getRelatedWordsByRoot(word, etymology, allRows) {
     const etymologyRoots = etymology.split(',').map(root => root.trim().toLowerCase());
     const relatedWords = allRows.filter(row => {
         const rowRoots = row.etymology.split(',').map(root => root.trim().toLowerCase());
-        return etymologyRoots.some(root => rowRoots.includes(root)) && row.word.toLowerCase() !== word.toLowerCase();
+        return etymologyRoots.every(root => rowRoots.includes(root)) && row.word.toLowerCase() !== word.toLowerCase();
     });
 
-    const relatedWordsList = relatedWords.map(row => `<a href="?search=${row.word}&id=${row.id}">${row.word}</a>`).join(', ');
+    const relatedWordsList = relatedWords.map(row => `<a href="?dictionaryword=${row.word}&dictionaryid=${row.id}">${row.word}</a>`).join(', ');
     const relatedWordsCount = relatedWords.length;
 
     return {
         count: relatedWordsCount,
         list: relatedWordsList
     };
-} 
+}
