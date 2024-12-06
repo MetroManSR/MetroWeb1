643e5da32b7b9d576a26a2566ad55d953b8c3737
MetroMan <services@metroman.me>
2024-12-06 15:48:11 -0300

    Update searchHighlight.js

--- assets/js/dictScripts/searchHighlight.js
+++ assets/js/dictScripts/searchHighlight.js
@@ -1,4 +1,5 @@
 export function highlight(text, term) {
+    if (!text || !term) return text;
     const regex = new RegExp(`(${term})`, 'gi');
     return text.replace(regex, '<mark>$1</mark>');
 }
