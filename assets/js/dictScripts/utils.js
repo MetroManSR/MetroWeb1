4270e34488bd93388a4f66ab358b09fb4a1fb007
MetroMan <services@metroman.me>
2024-12-05 21:18:18 -0300

    Update utils.js

--- assets/js/dictScripts/utils.js
+++ assets/js/dictScripts/utils.js
@@ -1,35 +1,9 @@
-export function calculateSimilarity(a, b) {
-    const lengthA = a.length;
-    const lengthB = b.length;
-    const d = [];
-
-    for (let i = 0; i <= lengthA; i++) {
-        d[i] = [i];
-    }
-
-    for (let j = 0; j <= lengthB; j++) {
-        d[0][j] = j;
-    }
-
-    for (let i = 1; i <= lengthA; i++) {
-        for (let j = 1; j <= lengthB; j++) {
-            if (a[i - 1] === b[j - 1]) {
-                d[i][j] = d[i - 1][j - 1];
-            } else {
-                d[i][j] = Math.min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + 1);
-            }
-        }
-    }
-
-    return (1 - (d[lengthA][lengthB] / Math.max(lengthA, lengthB))) * 100;
-}
-
-export function getRelatedWords(word, allRows) {
-    const relatedWords = allRows
-        .filter(row => calculateSimilarity(word, row.word) >= 90 && row.word.toLowerCase() !== word.toLowerCase())
-        .slice(0, 10)
-        .map(row => `<a href="?search=${row.word}&id=${row.id}">${row.word}</a>`)
-        .join(', ');
+export function getRelatedWordsByRoot(word, etymology, allRows) {
+    const etymologyRoots = etymology.split(',').map(root => root.trim().toLowerCase());
+    const relatedWords = allRows.filter(row => {
+        const rowRoots = row.etymology.split(',').map(root => root.trim().toLowerCase());
+        return etymologyRoots.some(root => rowRoots.includes(root)) && row.word.toLowerCase() !== word.toLowerCase();
+    }).map(row => `<a href="?search=${row.word}&id=${row.id}">${row.word}</a>`).join(', ');
 
     return relatedWords ? relatedWords : null;
 }
