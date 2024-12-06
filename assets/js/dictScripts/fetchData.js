c39386cbb6f47b524b4da281e68883246b1e33be
MetroMan <services@metroman.me>
2024-12-06 08:47:21 -0300

    Update fetchData.js

--- assets/js/dictScripts/fetchData.js
+++ assets/js/dictScripts/fetchData.js
@@ -1,17 +1,17 @@
-export async function fetchData(filePath) {
+export async function fetchData(filePath, type) {
     try {
         const response = await fetch(filePath);
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
         const data = await response.text();
-        return parseCSV(data);
+        return parseCSV(data, type);
     } catch (error) {
         console.error('Error loading CSV file:', error);
     }
 }
 
-function parseCSV(data) {
+function parseCSV(data, type) {
     const rows = [];
     const lines = data.split('\n').slice(1); // Remove the header row
     for (let i = 0; i < lines.length; i++) {
@@ -31,14 +31,35 @@ function parseCSV(data) {
         }
         columns.push(col.trim()); // Push the last column
 
-        const row = {
-            id: i + 1, // Assign a unique ID starting from 1
-            word: sanitizeHTML(columns[0]),
-            partOfSpeech: sanitizeHTML(columns[1]),
-            definition: sanitizeHTML(columns[2]),
-            explanation: sanitizeHTML(columns[3]),
-            etymology: sanitizeHTML(columns[4])
-        };
+        let row;
+        if (type === 'root') {
+            // Process as root
+            const raw = columns[0] || '';
+            const [root, rest] = raw.split(' = ');
+            const [translation, meta] = rest ? rest.split(' (') : ['', ''];
+            const [notes, origin] = meta ? meta.slice(0, -1).split(', ') : ['', ''];
+
+            row = {
+                id: i + 1, // Assign a unique ID starting from 1
+                word: sanitizeHTML(root ? root.trim() : ''),
+                translation: sanitizeHTML(translation ? translation.trim() : ''),
+                notes: sanitizeHTML(notes ? notes.trim() : ''),
+                etymology: sanitizeHTML(origin ? origin.trim() : ''),
+                type: 'root'
+            };
+        } else {
+            // Process as word
+            row = {
+                id: i + 1, // Assign a unique ID starting from 1
+                word: sanitizeHTML(columns[0]),
+                partOfSpeech: sanitizeHTML(columns[1]),
+                definition: sanitizeHTML(columns[2]),
+                explanation: sanitizeHTML(columns[3]),
+                etymology: sanitizeHTML(columns[4]),
+                type: 'word'
+            };
+        }
+
         rows.push(row);
     }
     return rows;
