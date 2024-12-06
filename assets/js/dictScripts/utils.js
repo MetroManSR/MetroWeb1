2c6d671ca0ab4c5ff72d79e2781ac56e336b4b93
MetroMan <services@metroman.me>
2024-12-06 15:48:27 -0300

    Update utils.js

--- assets/js/dictScripts/utils.js
+++ assets/js/dictScripts/utils.js
@@ -15,7 +15,7 @@ export function getRelatedWordsByRoot(word, etymology, allRows) {
 }
 
 export function highlight(text, term) {
-    if (!text) return '';
+    if (!text || !term) return text;
     const regex = new RegExp(`(${term})`, 'gi');
     return text.replace(regex, '<mark>$1</mark>');
 }
