27924ce167be1a0749c3f8c5598986cb2be0db04
MetroMan <services@metroman.me>
2024-12-09 12:40:19 -0300

    Update csvUtils.js

--- assets/js/dictScripts/csvUtils.js
+++ assets/js/dictScripts/csvUtils.js
@@ -1,14 +1,19 @@
 export function cleanData(data, type) {
+    console.log('Cleaning data for type:', type);
     return data.map((row, index) => {
+        console.log('Original row:', row);
+        
         const cleanedRow = {
             id: row.id || index, // Assign unique ID if missing
             type: type, // Identification of type (root or word)
-            title: sanitizeHTML(row.word ? row.word.trim() : ''), // Original root or word
-            meta: sanitizeHTML(row.translation ? row.translation.trim() : ''), // Translation to Spanish or English
+            title: sanitizeHTML(row.title ? row.title.trim() : ''), // Original root or word
+            partofspeech: sanitizeHTML(type === 'root' ? '' : row.partofspeech ? row.partofspeech.trim() : ''), // Empty for roots
+            meta: sanitizeHTML(row.meta ? row.meta.trim() : ''), // Translation to Spanish or English
             notes: sanitizeHTML(row.notes ? row.notes.trim() : ''), // Any notes left
-            morph: sanitizeHTML(type === 'root' ? (row.etymology ? row.etymology.trim() : '') : (row.partOfSpeech ? row.partOfSpeech.trim() : '')) // Etymology for roots, part of speech for words
+            morph: sanitizeHTML(row.morph ? row.morph.trim() : '') // Etymology for roots, part of speech for words
         };
 
+        console.log('Cleaned row:', cleanedRow);
         return cleanedRow;
     });
 }
