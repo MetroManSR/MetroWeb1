eb4ddc7c05ff808c066741e2c22ba558aee14e55
MetroMan <services@metroman.me>
2024-12-09 19:06:32 -0300

    Update fetchData.js

--- assets/js/dictScripts/fetchData.js
+++ assets/js/dictScripts/fetchData.js
@@ -6,17 +6,23 @@
  */
 export async function fetchData(url, type) {
     try {
+        console.log(`Fetching ${type} data from URL: ${url}`);
         const response = await fetch(url);
         const arrayBuffer = await response.arrayBuffer();
+        console.log(`Fetched ${type} data from URL: ${url}`);
+
         const data = new Uint8Array(arrayBuffer);
         const workbook = XLSX.read(data, { type: 'array' });
+        console.log(`${type} workbook:`, workbook);
 
         // Assuming the first sheet
         const firstSheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[firstSheetName];
+        console.log(`${type} worksheet:`, worksheet);
 
         // Converting the sheet to JSON
         const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+        console.log(`${type} JSON data:`, jsonData);
 
         return jsonData;
     } catch (error) {
