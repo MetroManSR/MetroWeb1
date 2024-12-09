80c9804a0dbc013fb73a7899085682cfb010431e
MetroMan <services@metroman.me>
2024-12-09 08:37:13 -0300

    Update init.js

--- assets/js/dictScripts/init.js
+++ assets/js/dictScripts/init.js
@@ -141,28 +141,28 @@ export function initializeEventListeners(allRows, allRowsById, rowsPerPage, filt
     }
 
     // Toggle filters visibility
-    const toggleFilterButton = document.getElementById('dict-toggle-filter-button');
-    if (toggleFilterButton) {
-        toggleFilterButton.addEventListener('click', () => {
-            const filterDropdown = document.getElementById('dict-filter-dropdown');
-            if (filterDropdown) {
-                if (filterDropdown.classList.contains('dict-hidden')) {
-                    filterDropdown.classList.remove('dict-hidden');
-                    filterDropdown.style.height = 'auto';
-                    const height = filterDropdown.clientHeight + 'px';
-                    filterDropdown.style.height = '0px';
-                    setTimeout(() => {
-                        filterDropdown.style.height = height;
-                    }, 10);
-                } else {
-                    filterDropdown.style.height = '0px';
-                    filterDropdown.addEventListener('transitionend', () => {
-                        filterDropdown.classList.add('dict-hidden');
-                    }, { once: true });
-                }
+const toggleFilterButton = document.getElementById('dict-toggle-filter-button');
+if (toggleFilterButton) {
+    toggleFilterButton.addEventListener('click', () => {
+        const filterDropdown = document.getElementById('dict-filter-dropdown');
+        if (filterDropdown) {
+            if (filterDropdown.classList.contains('dict-hidden')) {
+                filterDropdown.classList.remove('dict-hidden');
+                filterDropdown.style.height = 'auto';
+                const height = filterDropdown.clientHeight + 'px';
+                filterDropdown.style.height = '0px';
+                setTimeout(() => {
+                    filterDropdown.style.height = height;
+                }, 10);
+            } else {
+                filterDropdown.style.height = '0px';
+                filterDropdown.addEventListener('transitionend', () => {
+                    filterDropdown.classList.add('dict-hidden');
+                }, { once: true });
             }
-        });
-    }
+        }
+    });
+}
 
     // Initialize popups
     initAdvancedSearchPopup(allRows, rowsPerPage, displayPage);
