bd969365317c652a7ad7a4e7199d439967f5e3d4
MetroMan <services@metroman.me>
2024-12-10 08:20:47 -0300

    Update fetchData.js

--- assets/js/dictScripts/fetchData.js
+++ assets/js/dictScripts/fetchData.js
@@ -6,36 +6,30 @@
  */
 export async function fetchData(url, type) {
     try {
-        console.log(`Fetching ${type} data from URL: ${url}`);
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`Network response was not ok: ${response.statusText}`);
         }
         const arrayBuffer = await response.arrayBuffer();
-        console.log(`Fetched ${type} data from URL: ${url}`);
 
         const data = new Uint8Array(arrayBuffer);
         const workbook = XLSX.read(data, { type: 'array' });
-        console.log(`${type} workbook:`, workbook);
 
         // Assuming the first sheet
         const firstSheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[firstSheetName];
-        console.log(`${type} worksheet:`, worksheet);
 
         // Converting the sheet to JSON
         const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1, raw: false });
-        console.log(`${type} JSON data:`, jsonData);
 
         // Mapping the sheet data to objects
         return jsonData.map(row => {
             return row.reduce((acc, value, index) => {
-                acc[`col${index + 1}`] = decodeURIComponent(escape(value)); // Ensuring proper encoding
+                acc[`col${index + 1}`] = value;
                 return acc;
             }, {});
         });
     } catch (error) {
-        console.error(`Error fetching ${type} data:`, error);
         throw error;
     }
 }
