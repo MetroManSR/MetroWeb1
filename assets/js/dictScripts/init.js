0479d88ebf02111f75f384272d9000048f20a4ab
MetroMan <services@metroman.me>
2024-12-11 17:32:59 -0300

    Update init.js

--- assets/js/dictScripts/init.js
+++ assets/js/dictScripts/init.js
@@ -3,8 +3,7 @@ import { updatePagination } from './pagination.js';
 import { getTranslatedText } from './loadTexts.js';
 import { initAdvancedSearchPopup, initStatisticsPopup } from './popups.js'; // Ensure this is imported if used
 
-export function initializeEventListeners(rawAllRows, allRowsById, rowsPerPage, currentSortOrder, pendingChanges, processRows, displayPage) {
-    let allRows = await getRelatedWordsByRoot(rawAllRows);
+export function initializeEventListeners(allRows, allRowsById, rowsPerPage, currentSortOrder, pendingChanges, processRows, displayPage) {
     let currentPage = 1;
     let filteredRows = [];
     let previouslySelectedBox = null;
