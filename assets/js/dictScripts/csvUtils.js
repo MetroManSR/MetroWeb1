d6e706b992b42a71055a72cb4c4164192ef515ea
MetroMan <services@metroman.me>
2024-12-12 12:20:58 -0300

    Update csvUtils.js

--- assets/js/dictScripts/csvUtils.js
+++ assets/js/dictScripts/csvUtils.js
@@ -50,7 +50,7 @@ export async function cleanData(data, type) {
             cleanedRow.meta = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col3 ? row.col3.trim() : '') : row.col3 ? row.col3.trim() : ''); // Meta for words
             cleanedRow.notes = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col4 ? row.col4.trim() : '') : row.col4 ? row.col4.trim() : ''); // Notes for words
 
-            let morphData = Array.isArray(row.col5) ? row.col5.map(item => item.trim()) : [];
+            let morphData = row.col5 ? row.col5.trim().split(',').map(item => item.trim()) : [];
             cleanedRow.morph = morphData.map(item => {
                 // Create hyperlink if the root exists in the dictionary
                 const root = data.find(rootRow => rootRow.col1 && rootRow.col1.trim().toLowerCase() === item.trim().toLowerCase());
