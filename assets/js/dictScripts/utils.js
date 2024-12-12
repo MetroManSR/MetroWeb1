7ef60de6f961e5ef194e1ea96496d2b22c97c3fc
MetroMan <services@metroman.me>
2024-12-12 08:06:27 -0300

    Update utils.js

--- assets/js/dictScripts/utils.js
+++ assets/js/dictScripts/utils.js
@@ -1,11 +1,9 @@
 /**
  * Gets related words by root.
  *
- * @param {string} morph - The morph string of the current word.
  * @param {Array} allRows - The array of all dictionary rows.
  * @returns {Array} - The array of related words.
  */
-
 export async function getRelatedWordsByRoot(allRows) {
     // Calculate related words and derivative roots
     allRows.forEach(clnrow => {
@@ -27,7 +25,6 @@ export async function getRelatedWordsByRoot(allRows) {
                             }
                             return false;
                         });
-                        console.log(`Matching Roots for: ${clnrow.title} - ${matchingRoots}`);
                         relatedWords.push(...matchingRoots.map(r => `<a href="?wordid=${r.id}" style="color: green;">${r.title}</a>`));
                     }
                     // Logic for word type
@@ -38,8 +35,6 @@ export async function getRelatedWordsByRoot(allRows) {
                             }
                             return false;
                         });
-
-                        console.log(`Matching Words for: ${clnrow.title} - ${matchingWords}`);
                         relatedWords.push(...matchingWords.map(r => `<a href="?rootid=${r.id}" style="color: green;">${r.title}</a>`));
                     }
                 }
@@ -58,49 +53,7 @@ export function highlight(text, term) {
     return text.replace(regex, '<mark>$1</mark>');
 }
 
-// Other utility functions
-export function fetchData(file, type) {
-    return new Promise((resolve, reject) => {
-        fetch(file)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text();
-            })
-            .then(data => {
-                const parsedData = parseCSV(data);
-                resolve(parsedData.map(row => ({
-                    ...row,
-                    type
-                })));
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
-}
-
-function parseCSV(data) {
-    const rows = data.split('\n');
-    const headers = rows[0].split(',');
-    return rows.slice(1).map(row => {
-        const values = row.split(',');
-        return headers.reduce((obj, header, index) => {
-            obj[header.trim()] = values[index].trim();
-            return obj;
-        }, {});
-    });
-}
-
-export function cleanData(data, type) {
-    return data.filter(row => row.title && row.meta).map((row, index) => ({
-        ...row,
-        id: index + 1,
-        type
-    }));
-}
-
+// Utility function to sanitize HTML
 export function sanitizeHTML(html) {
     const tempDiv = document.createElement('div');
     tempDiv.textContent = html;
