40230da98bd105be1df6900cead5508030f7a1d4
MetroMan <services@metroman.me>
2024-12-13 15:43:30 -0300

    Update boxes.js

--- assets/js/dictScripts/boxes.js
+++ assets/js/dictScripts/boxes.js
@@ -53,13 +53,13 @@ export async function createDictionaryBox(row, allRows, searchTerm, exactMatch, 
 
     const wordElement = document.createElement('div');
     wordElement.classList.add('dictionary-box-title');
-    wordElement.innerHTML = highlight(row.title + (row.type !== 'root' ? ` (${partOfSpeechAbbr})` : ''), searchTerm);
+    wordElement.innerHTML = highlight(row.title + (row.type !== 'root' ? ` (${partOfSpeechAbbr})` : ''), searchTerm, searchIn);
 
     const hrElement = document.createElement('hr');
 
     const metaElement = document.createElement('div');
     metaElement.classList.add('dictionary-box-meta');
-    metaElement.innerHTML = highlight(row.meta, searchTerm);
+    metaElement.innerHTML = highlight(row.meta, searchTerm, searchIn);
 
     const contentBox = document.createElement('div');
     contentBox.classList.add('dictionary-box-content');
@@ -72,13 +72,13 @@ export async function createDictionaryBox(row, allRows, searchTerm, exactMatch, 
     
     // Display morphology for words and etymology for roots
     if (row.type === 'root') {
-        metaElement.innerHTML = `<strong>${await getTranslatedText('translation', language)}:</strong> ${highlight(row.meta, searchTerm)}`;
-        notesElement.innerHTML = `<strong>${await getTranslatedText('etymology', language)}:</strong> ${highlight(row.notes || '', searchTerm)}`;
+        metaElement.innerHTML = `<strong>${await getTranslatedText('translation', language)}:</strong> ${highlight(row.meta, searchTerm, searchIn)}`;
+        notesElement.innerHTML = `<strong>${await getTranslatedText('etymology', language)}:</strong> ${highlight(row.notes || '', searchTerm, searchIn)}`;
         contentBox.appendChild(metaElement);
         contentBox.appendChild(notesElement);
     } else {
-        metaElement.innerHTML = `<strong>${await getTranslatedText('translation', language)}:</strong> ${highlight(row.meta, searchTerm)}`;
-        notesElement.innerHTML = `<strong>${await getTranslatedText('notes', language)}:</strong> ${highlight(row.notes || '', searchTerm)}`;
+        metaElement.innerHTML = `<strong>${await getTranslatedText('translation', language)}:</strong> ${highlight(row.meta, searchTerm, searchIn)}`;
+        notesElement.innerHTML = `<strong>${await getTranslatedText('notes', language)}:</strong> ${highlight(row.notes || '', searchTerm, searchIn)}`;
         contentBox.appendChild(metaElement);
         contentBox.appendChild(notesElement);
 
@@ -87,8 +87,8 @@ export async function createDictionaryBox(row, allRows, searchTerm, exactMatch, 
             row.morph.forEach((morphTitle, index) => {
                 const matchingRoot = allRows.find(r => r.meta.toLowerCase() === morphTitle.toLowerCase() && r.type === 'root');
                 morphElement.innerHTML += matchingRoot 
-                    ? createHyperlink(morphTitle, searchTerm, allRows) 
-                    : highlight(morphTitle, searchTerm);
+                    ? createHyperlink(morphTitle, searchTerm, allRows, searchIn) 
+                    : highlight(morphTitle, searchTerm, searchIn);
                 if (index < row.morph.length - 1) {
                     morphElement.innerHTML += ', ';
                 }
