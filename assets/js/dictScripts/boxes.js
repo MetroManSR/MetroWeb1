29add0ccc2bc95bbaa98fc3fcb464c39c1a143ac
MetroMan <services@metroman.me>
2024-12-14 09:22:30 -0300

    Update boxes.js

--- assets/js/dictScripts/boxes.js
+++ assets/js/dictScripts/boxes.js
@@ -74,9 +74,12 @@ export async function createDictionaryBox(row, allRows, searchTerm, exactMatch, 
     // Display morphology for words and etymology for roots
     if (row.type === 'root') {
         metaElement.innerHTML = `<strong>${await getTranslatedText('translation', language)}:</strong> ${highlight(row.meta, searchTerm, searchIn, row)}`;
-        notesElement.innerHTML = `<strong>${await getTranslatedText('etymology', language)}:</strong> ${highlight(row.notes || '', searchTerm, searchIn, row)}`;
+        notesElement.innerHTML = `<strong>${await getTranslatedText('notes', language)}:</strong> ${highlight(row.notes || '', searchTerm, searchIn, row)}`;
+        morphElement.innerHTML = `<strong>${await getTranslatedText('etimology', language)}:</strong> ${highlight(row.morph.join(', ') || '', searchTerm, searchIn, row)}`;        
         contentBox.appendChild(metaElement);
         contentBox.appendChild(notesElement);
+        contentBox.appendChild(morphElement);
+ 
     } else {
         metaElement.innerHTML = `<strong>${await getTranslatedText('translation', language)}:</strong> ${highlight(row.meta, searchTerm, searchIn, row)}`;
         notesElement.innerHTML = `<strong>${await getTranslatedText('notes', language)}:</strong> ${highlight(row.notes || '', searchTerm, searchIn, row)}`;
