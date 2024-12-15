8d58dba80f22dc9e49bee1294af79128e3fda488
MetroMan <services@metroman.me>
2024-12-15 12:41:52 -0300

    Update initButtonEventListeners.js

--- assets/js/dictScripts/initButtonEventListeners.js
+++ assets/js/dictScripts/initButtonEventListeners.js
@@ -35,13 +35,13 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
         pendingChangesElement.style.display = 'block';
     }
    
-    initializeFormEventListeners(allRows, pendingChanges, rowsPerPage, displayPage);
-    updatePendingChangesList(pendingChanges, language);
+    initializeFormEventListeners(allRows, rowsPerPage);
+    updatePendingChangesList(language);
     const orderBySelect = document.getElementById('dict-order-by-select');
     if (orderBySelect) {
         orderBySelect.addEventListener('change', () => {
             pendingChanges.sortOrder = orderBySelect.value;
-            updatePendingChangesList(pendingChanges, language);
+            updatePendingChangesList(language);
             universalPendingChanges = pendingChanges;
         });
     }
@@ -49,7 +49,7 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
     if (filterSelect) {
         filterSelect.addEventListener('change', () => {
             pendingChanges.filters = Array.from(filterSelect.selectedOptions).map(option => option.value);
-            updatePendingChangesList(pendingChanges, language);
+            updatePendingChangesList(language);
             universalPendingChanges = pendingChanges;
         });
     }
@@ -64,7 +64,7 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
     const advancedSearchButton = document.getElementById('dict-advanced-search-button');
     if (advancedSearchButton) {
         advancedSearchButton.addEventListener('click', () => {
-            initAdvancedSearchPopup(allRows, rowsPerPage, displayPage, pendingChanges);
+            initAdvancedSearchPopup(allRows, rowsPerPage);
         });
     }
     const viewStatisticsButton = document.getElementById('dict-view-statistics-button');
@@ -76,7 +76,7 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
     const applySettingsButton = document.getElementById('dict-apply-settings-button');
     if (applySettingsButton) {
     applySettingsButton.addEventListener('click', async () => {
-        await processAllSettings(pendingChanges, allRows, rowsPerPage, displayPage, currentPage, pendingChanges.sortOrder);
+        await processAllSettings(allRows, rowsPerPage, currentPage, pendingChanges.sortOrder);
     });
     }
     const cleanSettingsButton = document.getElementById('dict-clear-settings-button');
@@ -108,7 +108,7 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
                 option.selected = false;
             });
             updatePendingChangesList(pendingChanges, language);
-            await processAllSettings(pendingChanges, allRows, pendingChanges.rowsPerPage, displayPage, 1, pendingChanges.sortOrder);
+            await processAllSettings(allRows, pendingChanges.rowsPerPage, currentPage, pendingChanges.sortOrder);
             universalPendingChanges = pendingChanges;
             // Remove URL parameters without reloading the page
             history.pushState({}, document.title, window.location.pathname);
@@ -121,7 +121,7 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
             document.getElementById('dict-search-input').value = '';
             updatePendingChangesList(pendingChanges, language);
             universalPendingChanges = pendingChanges;
-            await processAllSettings(pendingChanges, allRows, rowsPerPage, displayPage, currentPage, pendingChanges.sortOrder);
+            await processAllSettings(allRows, rowsPerPage, currentPage, pendingChanges.sortOrder);
             // Remove URL parameters without reloading the page
             history.pushState({}, document.title, window.location.pathname);
         });
@@ -145,7 +145,7 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
         }
         const totalPages = Math.ceil(filteredRows.length / rowsPerPage);
         updatePagination(currentPage, totalPages);
-        displayPage(currentPage, rowsPerPage, pendingChanges.searchTerm, pendingChanges.searchIn, pendingChanges.exactMatch, filteredRows, allRows);
+        displayPage(currentPage, rowsPerPage, pendingChanges.searchTerm, pendingChanges.searchIn, pendingChanges.exactMatch, allRows);
     }
     if (filteredRows) {
 
