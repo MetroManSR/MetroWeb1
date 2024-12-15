df10af4ad34b298593ecdd1b433d2e6fc217ed28
MetroMan <services@metroman.me>
2024-12-15 14:26:54 -0300

    Delete assets/js/dictScripts/searchHighlight.js

--- assets/js/dictScripts/searchHighlight.js
+++
@@ -1,5 +0,0 @@
-export function highlight(text, term) {
-    if (!text || !term) return text;
-    const regex = new RegExp(`(${term})`, 'gi');
-    return text.replace(regex, '<mark style="background-color: yellow;">$1</mark>');
-}
