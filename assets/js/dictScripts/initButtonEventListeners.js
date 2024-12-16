2775fbefeb830115b404d30c80d98145c4a0ffe4
MetroMan <services@metroman.me>
2024-12-16 10:22:04 -0300

    Update initButtonEventListeners.js

--- assets/js/dictScripts/initButtonEventListeners.js
+++ assets/js/dictScripts/initButtonEventListeners.js
@@ -4,7 +4,6 @@ import {
 } from './processRows.js';
 import {
     universalPendingChanges,
-    updateUniversalPendingChanges,
     updatePendingChangesList,
     defaultPendingChanges,
     initializeFormEventListeners 
@@ -60,7 +59,14 @@ if (toggleFilterButton) {
         const filterSortingContainer = document.getElementById('dict-filter-sorting-container');
         if (filterSortingContainer) {
             console.log('Button clicked. Toggling classes...');
-            filterSortingContainer.classList.toggle('active');
+            
+            filterSortingContainer.classList.toggle('dict-filter-cont-hidden');
+            console.log('After toggle, hidden class: ', filterSortingContainer.classList.contains('dict-filter-cont-hidden'));
+            
+            filterSortingContainer.classList.toggle('dict-filter-cont-visible');
+            console.log('After toggle, visible class: ', filterSortingContainer.classList.contains('dict-filter-cont-visible'));
+            
+            // Print current classes
             console.log('Current classes: ', filterSortingContainer.className);
         } else {
             console.error('filterSortingContainer not found');
