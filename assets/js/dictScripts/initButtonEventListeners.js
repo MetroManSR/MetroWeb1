7a50ce7ddf019223b814dcda30bf03fd4943badd
MetroMan <services@metroman.me>
2024-12-16 13:45:44 -0300

    Update initButtonEventListeners.js

--- assets/js/dictScripts/initButtonEventListeners.js
+++ assets/js/dictScripts/initButtonEventListeners.js
@@ -1,42 +1,25 @@
-import {
-    processAllSettings, 
-    displayPage
-} from './processRows.js';
-import {
-    universalPendingChanges,
-    updatePendingChangesList,
-    defaultPendingChanges,
-    initializeFormEventListeners 
-} from './initFormEventListeners.js';
-import {
-    initAdvancedSearchPopup,
-    initStatisticsPopup
-} from './popups.js';
+import { processAllSettings, displayPage } from './processRows.js';
+import { universalPendingChanges, updatePendingChangesList, defaultPendingChanges, initializeFormEventListeners } from './initFormEventListeners.js';
+import { initAdvancedSearchPopup, initStatisticsPopup } from './popups.js';
 import { updatePagination } from './pagination.js';
-import { filteredRows } from '../mainDict.js';
-
-import {
-    boxClickListener
-} from './boxEvents.js';
-
 
 export async function initializeButtonEventListeners(allRows, rowsPerPage, currentSortOrder) {
-    
-    console.log('Initializing Button Event Listeners'); 
+    console.log('Initializing Button Event Listeners');
     
     const language = document.querySelector('meta[name="language"]').content || 'en';
     let currentPage = 1;
-    // Ensure pendingChanges list is visible on page load
+    let pendingChanges = universalPendingChanges ? universalPendingChanges : defaultPendingChanges;
+    
+    // Ensure pending changes are visible on page load
     const pendingChangesElement = document.getElementById('dict-pending-changes');
     if (pendingChangesElement) {
         pendingChangesElement.style.display = 'block';
     }
-   
+
     initializeFormEventListeners(allRows, rowsPerPage);
 
-    let pendingChanges = universalPendingChanges ? universalPendingChanges : defaultPendingChanges ;
-    
     updatePendingChangesList(language);
+
     const orderBySelect = document.getElementById('dict-order-by-select');
     if (orderBySelect) {
         orderBySelect.addEventListener('change', () => {
@@ -45,6 +28,7 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
             updateUniversalPendingChanges(pendingChanges);
         });
     }
+
     const filterSelect = document.getElementById('dict-word-filter');
     if (filterSelect) {
         filterSelect.addEventListener('change', () => {
@@ -55,49 +39,45 @@ export async function initializeButtonEventListeners(allRows, rowsPerPage, curre
     }
 
     const toggleFilterButton = document.getElementById('dict-toggle-filter-button');
-if (toggleFilterButton) {
-    toggleFilterButton.addEventListener('click', () => {
-        const filterSortingContainer = document.getElementById('dict-filter-sorting-container');
-        if (filterSortingContainer) {
-            console.log('Button clicked. Toggling classes...');
-            
-            filterSortingContainer.classList.toggle('dict-filter-cont-hidden');
-            console.log('After toggle, hidden class: ', filterSortingContainer.classList.contains('dict-filter-cont-hidden'));
-            
-            filterSortingContainer.classList.toggle('dict-filter-cont-visible');
-            console.log('After toggle, visible class: ', filterSortingContainer.classList.contains('dict-filter-cont-visible'));
-            
-            // Print current classes
-            console.log('Current classes: ', filterSortingContainer.className);
-        } else {
-            console.error('filterSortingContainer not found');
-        }
-    });
-} else {
-    console.error('toggleFilterButton not found');
-} 
-    
+    if (toggleFilterButton) {
+        toggleFilterButton.addEventListener('click', () => {
+            const filterSortingContainer = document.getElementById('dict-filter-sorting-container');
+            if (filterSortingContainer) {
+                console.log('Button clicked. Toggling classes...');
+                filterSortingContainer.classList.toggle('active');
+                console.log('Current classes: ', filterSortingContainer.className);
+            } else {
+                console.error('filterSortingContainer not found');
+            }
+        });
+    } else {
+        console.error('toggleFilterButton not found');
+    }
+
     const advancedSearchButton = document.getElementById('dict-advanced-search-button');
     if (advancedSearchButton) {
         advancedSearchButton.addEventListener('click', () => {
-            initAdvancedSearchPopup(allRows, rowsPerPage);
+            initAdvancedSearchPopup(allRows, rowsPerPage, language);
         });
     }
+
     const viewStatisticsButton = document.getElementById('dict-view-statistics-button');
     if (viewStatisticsButton) {
         viewStatisticsButton.addEventListener('click', () => {
             initStatisticsPopup(allRows);
         });
     }
+
     const applySettingsButton = document.getElementById('dict-apply-settings-button');
     if (applySettingsButton) {
-    applySettingsButton.addEventListener('click', async () => {
-        await processAllSettings(allRows, rowsPerPage, currentPage, pendingChanges.sortOrder);
-    });
+        applySettingsButton.addEventListener('click', async () => {
+            await processAllSettings(allRows, rowsPerPage, currentPage, pendingChanges.sortOrder);
+        });
     }
-    const cleanSettingsButton = document.getElementById('dict-clear-settings-button');
-    if (cleanSettingsButton) {
-        cleanSettingsButton.addEventListener('click', async () => {
+
+    const clearSettingsButton = document.getElementById('dict-clear-settings-button');
+    if (clearSettingsButton) {
+        clearSettingsButton.addEventListener('click', async () => {
             pendingChanges = {
                 searchTerm: '',
                 exactMatch: false,
@@ -109,7 +89,7 @@ if (toggleFilterButton) {
                 },
                 filters: [],
                 rowsPerPage: 20,
-                sortOrder: 'titleup' // Default sort order
+                sortOrder: 'titleup'
             };
             // Reset form fields in the advanced search popup
             document.getElementById('dict-search-input').value = '';
@@ -130,9 +110,10 @@ if (toggleFilterButton) {
             history.pushState({}, document.title, window.location.pathname);
         });
     }
-    const cleanSearchButton = document.getElementById('dict-clear-search-button');
-    if (cleanSearchButton) {
-        cleanSearchButton.addEventListener('click', async () => {
+
+    const clearSearchButton = document.getElementById('dict-clear-search-button');
+    if (clearSearchButton) {
+        clearSearchButton.addEventListener('click', async () => {
             pendingChanges.searchTerm = '';
             document.getElementById('dict-search-input').value = '';
             updatePendingChangesList(pendingChanges, language);
@@ -142,8 +123,10 @@ if (toggleFilterButton) {
             history.pushState({}, document.title, window.location.pathname);
         });
     }
+
     const searchInput = document.getElementById('dict-search-input');
     const predictionBox = document.getElementById('dict-search-predictions');
+
     document.querySelectorAll('.pagination-button').forEach(button => {
         button.addEventListener('click', (e) => {
             const targetPage = parseInt(e.target.dataset.page, 10);
@@ -163,10 +146,10 @@ if (toggleFilterButton) {
         updatePagination(currentPage, totalPages);
         displayPage(currentPage, rowsPerPage, pendingChanges.searchTerm, pendingChanges.searchIn, pendingChanges.exactMatch, allRows);
     }
+    
+    // Initial page load
     if (filteredRows) {
-
-      navigateToPage(1);
-
+        navigateToPage(1);
     }
 
     console.log('Button Event Listeners initialized');
