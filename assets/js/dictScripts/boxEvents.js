1ed785aa1520b7a3e7100cc11520df7d3e2ba0f9
MetroMan <services@metroman.me>
2024-12-19 13:40:53 -0300

    Update boxEvents.js

--- assets/js/dictScripts/boxEvents.js
+++ assets/js/dictScripts/boxEvents.js
@@ -5,7 +5,10 @@ import { universalPendingChanges} from './initFormEventListeners.js';
 let previouslySelectedBox = null;
 let lastClickTime = 0;
 
-export async function loadInfoBox(box, row) {
+export async function loadInfoBox(box, row) {  
+        
+    const language = document.querySelector('meta[name="language"]').content || 'en';
+
     // Info button
     const infoButton = document.createElement('button');
     infoButton.className = 'info-button';
@@ -30,13 +33,13 @@ export async function loadInfoBox(box, row) {
 
     // Add event listeners
     warningIcon.addEventListener('click', async () => {
-        const message = `${await getTranslatedText('bugCopyPaste01')}${row.title} [${row.id}]${getTranslatedText('bugCopyPaste02')}`;
+        const message = `${await getTranslatedText('bugCopyPaste01', language)}${row.title} [${row.id}]${await getTranslatedText('bugCopyPaste02', language)}`;
         copyToClipboard(message);
         showTooltip('Copied to clipboard! Paste this in the help channel of the discord server of Balkeon.');
     });
 
     suggestionIcon.addEventListener('click', async () =>  {
-        const message = `${await getTranslatedText('ideaCopyPaste01')}${row.title} [${row.id}]${getTranslatedText('ideaCopyPaste02')}`;
+        const message = `${await getTranslatedText('ideaCopyPaste01',  language)}${row.title} [${row.id}]${await getTranslatedText('ideaCopyPaste02', language)}`;
         copyToClipboard(message);
         showTooltip('Copied to clipboard! Paste this in the help channel of the discord server of Balkeon.');
     });
